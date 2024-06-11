4b4a83c7a6e9dd81c374341b5138f18fbc739ba7
Cedrick Campoto <cedrick.campoto@yahoo.com>
2024-06-11 09:26:47 +0800

    update

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -10,38 +10,43 @@ export const load = (async () => {
 
 export const actions = {
 	login: async (event) => {
-		const data = await event.request.formData();
+		try {
+			const data = await event.request.formData();
 
-		const username: string = data.get('username') as any;
-		const password: string = data.get('password') as any;
+			const username: string = data.get('username') as any;
+			const password: string = data.get('password') as any;
 
-		const find = await Models.Accounts.findOne({
-			raw: true,
-			where: {
-				username: username
+			const find = await Models.Accounts.findOne({
+				raw: true,
+				where: {
+					username: username
+				}
+			});
+
+			if (!find) {
+				throw new Error('account not found');
 			}
-		});
+			console.log(18, find);
 
-		if (!find) {
-			return fail(400, { message: 'Account not found', error: true });
+			const compare = bcrypt.compareSync(password, find.password);
+
+			if (!compare) {
+				throw new Error('wrong password');
+			}
+
+			const token = jwt.sign({}, 'deployer', { expiresIn: `${15 * 60 * 1000}` });
+
+			event.cookies.set('x-token', token, {
+				httpOnly: true,
+				path: '/',
+				sameSite: 'strict',
+				secure: true
+			});
+
+			return redirect(302, '/redirect?redirect_url=/');
+		} catch (err) {
+			console.log(err);
+			return fail(400, { message: err.message, error: true });
 		}
-		console.log(18, find);
-
-		const compare = bcrypt.compareSync(password, find.password);
-
-		if (!compare) {
-			return fail(400, { message: 'Incorrect password', error: true });
-		}
-
-		const token = jwt.sign({}, 'deployer', { expiresIn: `${15 * 60 * 1000}` });
-
-		event.cookies.set('x-token', token, {
-			httpOnly: true,
-			path: '/',
-			sameSite: 'strict',
-			secure: true
-		});
-
-		return redirect(302, '/redirect?redirect_url=/');
 	}
 };
