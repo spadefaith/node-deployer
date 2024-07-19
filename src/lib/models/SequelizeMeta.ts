e5c350d1f748ef48d9fe522be0408d3b260ac353
Cedrick Campoto <cedrick.campoto@yahoo.com>
2024-07-20 00:58:27 +0800

    update

--- src/lib/models/SequelizeMeta.ts
+++ src/lib/models/SequelizeMeta.ts
@@ -2,38 +2,41 @@ import * as Sequelize from 'sequelize';
 import { DataTypes, Model, Optional } from 'sequelize';
 
 export interface SequelizeMetaAttributes {
-  name: string;
+	name: string;
 }
 
-export type SequelizeMetaPk = "name";
+export type SequelizeMetaPk = 'name';
 export type SequelizeMetaId = SequelizeMeta[SequelizeMetaPk];
 export type SequelizeMetaCreationAttributes = SequelizeMetaAttributes;
 
-export class SequelizeMeta extends Model<SequelizeMetaAttributes, SequelizeMetaCreationAttributes> implements SequelizeMetaAttributes {
-  name!: string;
+export class SequelizeMeta
+	extends Model<SequelizeMetaAttributes, SequelizeMetaCreationAttributes>
+	implements SequelizeMetaAttributes
+{
+	name!: string;
 
-
-  static initModel(sequelize: Sequelize.Sequelize): typeof SequelizeMeta {
-    return SequelizeMeta.init({
-    name: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-      primaryKey: true,
-      unique: true
-    }
-  }, {
-    sequelize,
-    tableName: 'SequelizeMeta',
-    timestamps: false,
-    indexes: [
-      {
-        name: "sqlite_autoindex_SequelizeMeta_1",
-        unique: true,
-        fields: [
-          { name: "name" },
-        ]
-      },
-    ]
-  });
-  }
+	static initModel(sequelize: Sequelize.Sequelize): typeof SequelizeMeta {
+		return SequelizeMeta.init(
+			{
+				name: {
+					type: DataTypes.STRING(255),
+					allowNull: false,
+					primaryKey: true,
+					unique: true
+				}
+			},
+			{
+				sequelize,
+				tableName: 'SequelizeMeta',
+				timestamps: false,
+				indexes: [
+					{
+						name: 'sqlite_autoindex_SequelizeMeta_1',
+						unique: true,
+						fields: [{ name: 'name' }]
+					}
+				]
+			}
+		);
+	}
 }
